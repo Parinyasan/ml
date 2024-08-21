646b5d8ea7682dd99c109e5209c448734b3b65fc
Parinya Sanguansat <36033770+Parinyasan@users.noreply.github.com>
2024-08-21 14:45:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
   knn.fit(x_train, y_train)
   y_pred = knn.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
 if classifier == 'SVM':
   svm = SVC()
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
